06494a93c539844b21d2f5bad213de69b041813e
uruhayato373 <uruhayato373@gmail.com>
2025-10-13 15:45:13 +0900

    test

--- src/components/choropleth/index.ts
+++ src/components/choropleth/index.ts
@@ -2,6 +2,4 @@
  * コロプレス地図表示機能のコンポーネントエクスポート
  */
 
-export { CategorySidebar } from "./CategorySidebar";
 export { CategoryPageClient } from "./CategoryPageClient";
-export { ChoroplethDataDisplayClient } from "./ChoroplethDataDisplayClient";
