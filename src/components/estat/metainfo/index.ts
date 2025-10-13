fb361392ac86050d24df641c36e1f9d913f56b8f
uruhayato373 <uruhayato373@gmail.com>
2025-10-13 13:35:16 +0900

    test

--- src/components/estat/metainfo/index.ts
+++ src/components/estat/metainfo/index.ts
@@ -1,7 +1,6 @@
 // メタデータ関連コンポーネントのエクスポート
 export { PageHeader as EstatMetaInfoPageHeader } from "./Header";
 export { EstatMetaInfoSidebar, SavedEstatMetaInfoList } from "./Sidebar";
-export { default as EstatMetaInfoActions } from "./Actions";
 export { EstatMetaInfoDisplay } from "./Display";
 export { MetaInfoFetcher as EstatMetaInfoFetcher } from "./Fetcher";
 export { default as EstatMetainfoPage } from "./EstatMetainfoPage";
